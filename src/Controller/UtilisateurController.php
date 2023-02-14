e1efb27097148fe1301b3e2cbd30cd36dcea3ebd
zouhourkharraf <zouhour.kharraf1@esprit.tn>
2023-02-14 18:38:20 +0100

    finalisation de la partie affichage back office

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -9,25 +9,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UtilisateurController extends AbstractController
 {
-    #[Route('/utilisateur', name: 'app_utilisateur')]
+    #[Route('/listutilisateur', name: 'liste_utilisateur')]
     public function index(): Response
     {
-        return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
-        ]);
+        return $this->render('utilisateur/GererUtilBack.html.twig');
     }
 
+
+
     #[Route('/listadmin', name: 'liste_administrateurs')]
-    public function LiteDesAdministrateurs(UtilisateurRepository $utilisateurRepository)
+    public function AfficherAdministrateurs(UtilisateurRepository $utilisateurRepository)
     {
-        $ListAdmin = $utilisateurRepository->findAll();
+        $ListAdmin = $utilisateurRepository->findBy(array('role_util' => 'administrateur'));
         return $this->render('utilisateur/listadminback.html.twig', array("liste_administrateurs" => $ListAdmin));
     }
 
-    #[Route('/routetest', name: 'route_test')]
-    public function fonction_de_test()
+    #[Route('/listprof', name: 'liste_enseignants')]
+    public function AfficherEnseignants(UtilisateurRepository $utilisateurRepository)
     {
-        $ListAdmin = NULL;
+        $ListEnseignant = $utilisateurRepository->findBy(array('role_util' => 'enseignant'));
+        return $this->render('utilisateur/listenseignantback.html.twig', array("liste_enseignants" => $ListEnseignant));
+    }
+
+    #[Route('/listeleve', name: 'liste_eleves')]
+    public function AfficherEleves(UtilisateurRepository $utilisateurRepository)
+    {
+        $ListEleves = $utilisateurRepository->findBy(array('role_util' => 'élève'));
+        return $this->render('utilisateur/listeleveback.html.twig', array("liste_eleves" => $ListEleves));
+    }
+
+
+
+    //NB: ********************* fonction de test ****************
+    #[Route('/routetest', name: 'route_test')]
+    public function fonction_de_test(UtilisateurRepository $utilisateurRepository)
+    {
+        $ListAdmin = $utilisateurRepository->findAll();
         return $this->render('utilisateur/test.html.twig', array("liste_administrateurs" => $ListAdmin));
     }
 }
