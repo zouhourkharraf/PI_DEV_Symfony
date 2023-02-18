71abc2945d4df4e08b2b03f2ad354d649b4fb46f
sarrakabbous <sarra.kabbous@esprit.tn>
2023-02-18 12:44:56 +0000

    version 2

--- src/Controller/DonController.php
+++ src/Controller/DonController.php
@@ -23,26 +23,53 @@ class DonController extends AbstractController
             'controller_name' => 'DonController',
         ]);
     }
+
     #[Route('/adddon', name: 'app_adddon')]
-    public function addStudent(\Doctrine\Persistence\ManagerRegistry $doctrine,Request $request)
+    public function addDon(\Doctrine\Persistence\ManagerRegistry $doctrine, Request $request)
     {
-        $don= new Don();
-        $form= $this->createForm(DonType::class,$don);
+        $don = new Don();
+        $form = $this->createForm(DonType::class, $don);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $doctrine->getManager();
             $em->persist($don);
             $em->flush();
-            return  $this->redirectToRoute("app_adddon");
+            return $this->redirectToRoute("app_adddon");
         }
         return $this->renderForm("don/addDon.html.twig",
-            array("formDon"=>$form));
+            array("formDon" => $form));
     }
 
     #[Route('/dons', name: 'app_dons')]
     public function listdon(DonRepository $repository): Response
     {
-        $dons= $repository->findAll();
-        return $this->render("don/listdons.html.twig",array("tabdons"=>$dons));
+        $dons = $repository->findAll();
+        return $this->render("don/listdons.html.twig", array("tabdons" => $dons));
+    }
+
+    #[Route('/updateDon/{id}', name: 'app_updateDon')]
+    public function updateDon(DonRepository $repository, $id, ManagerRegistry $doctrine, Request $request)
+    {
+        $don = $repository->find($id);
+        $form = $this->createForm(DonType::class, $don);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $em = $doctrine->getManager();
+            $em->flush();
+            return $this->redirectToRoute("app_dons");
+        }
+        return $this->renderForm('Don/update_don.html.twig', ['form' => $form]);
+
+    }
+
+    #[Route('/removeDon/{id}', name: 'app_removeDon')]
+    public function deleteDon(ManagerRegistry $doctrine,$id,DonRepository $repository)
+    {
+        $don= $repository->find($id);
+        $em= $doctrine->getManager();
+        $em->remove($don);
+        $em->flush();
+        return $this->redirectToRoute("app_dons");
+
     }
 }
